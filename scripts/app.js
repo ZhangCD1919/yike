846745bf7624c6730c1a6b536ff9dcf13264ecc4
ZhangCD1919 <zhangcaidie1919@163.com>
2017-03-29 22:18:27 +0800

    导航功能基本完成

--- scripts/app.js
+++ scripts/app.js
@@ -1,85 +1,58 @@
+var yike = angular.module("yike", ["ngRoute"]);
 
-// 入口文件
-var App = angular.module('App', ['ngRoute', 'controllers', 'directives', 'services']);
 
-// 配置路由
-App.config(['$routeProvider', function($routeProvider) {
-	// 今日一刻
-	$routeProvider.when('/', {
-		templateUrl: 'views/today.html',
-		controller: 'TodayController'
-	})
-	// 往期内容
-	.when('/older', {
-		templateUrl: 'views/older.html',
-		controller: 'OlderController'
-	})
-	// 热门作者
-	.when('/author', {
-		templateUrl: 'views/author.html',
-		controller: 'AuthorController'
-	})
-	// 个人中心（作者）
-	.when('/center', {
-		templateUrl: 'views/center.html'
-	})
-	// 栏目浏览
-	.when('/category', {
-		templateUrl: 'views/category.html'
-	})
-	// 栏目列表
-	.when('/list', {
-		templateUrl: 'views/list.html'
-	})
-	// 我的喜欢
-	.when('/favourite', {
-		templateUrl: 'views/favourite.html'
-	})
-	// 设置
-	.when('/settings', {
-		templateUrl: 'views/settings.html'
-	});
-
+// 路由设置
+yike.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/today', {
+        templateUrl: "./views/today.html"
+    }).when("/older", {
+        templateUrl: "./views/older.html"
+    }).when("/author", {
+        templateUrl: "./views/author.html"
+    }).when("/category", {
+        templateUrl: "./views/category.html"
+    }).when("/settings", {
+        templateUrl: "./views/settings.html"
+    }).otherwise({
+        redirectTo: '/today'
+    })
 }]);
 
-App.run(['$rootScope', function ($rootScope, collapse) {
-	// 显示加载图标
-	$rootScope.loaded = false;
+// 导航遍历数据
+yike.controller("navsCtrl", ["$scope", function ($scope) {
+    $scope.navs = [
+        {text: "今日一刻", icon: "icon-home", link: "#!/today"},
+        {text: "往期内容", icon: "icon-file-empty", link: "#!/older"},
+        {text: "热门作者", icon: "icon-pencil", link: "#!/author"},
+        {text: "栏目浏览", icon: "icon-menu", link: "#!/category"},
+        {text: "设置", icon: "icon-cog", link: "#!/settings"}
+    ]
+}]);
 
-	// 侧边栏打开状态（未打开）
-	$rootScope.collapsed = false;
 
-	// 侧边栏索引
-	$rootScope.index = 0;
+// 根作用域的参数
+yike.run(["$rootScope", function ($rootScope) {
+    // collapsed控制模块的移动
+    $rootScope.collapsed = false;
+    $rootScope.toggle = function () {
+        $rootScope.collapsed = !$rootScope.collapsed;
 
-	$rootScope.toggle = function (index) {
-		// 切换侧边栏状态
-		$rootScope.collapsed = !$rootScope.collapsed;
+        // 获取导航菜单
+        var navs = document.querySelectorAll(".navs dd");
+        if ($rootScope.collapsed) {
+            for (var i = 0; i < navs.length; i++) {
+                navs[i].style.transform = "translate(0)";
+                navs[i].style.transitionDuration = 0.15 * i + "s";
+                navs[i].style.transitionDelay = "0.3s";
+            }
+        } else {
+            for (var i = 0; i < navs.length; i++) {
+                navs[i].style.transform = "translate(-100%)";
+                navs[i].style.transitionDuration = 0.15 * ( navs.length-i) + "s";
+                navs[i].style.transitionDelay = "";
+            }
+        }
 
-		// 获取所有导航
-		var navs = document.querySelectorAll('.navs dd');
 
-		// 设置当前导航状态
-		document.querySelector('.navs dd.active').classList.remove('active');
-
-		navs[$rootScope.index].classList.add('active');
-
-		// 设置动国效果
-		if($rootScope.collapsed) {
-
-			for(var i=0; i<navs.length; i++) {
-				navs[i].style.transitionDelay = '0.2s';
-				navs[i].style.transform = 'translate(0)';
-				navs[i].style.transitionDuration = (i + 1) * 0.15 + 's';
-			}
-
-		} else {
-			
-			for(var i=navs.length - 1; i>=0; i--) {
-				navs[i].style.transitionDelay = '';
-				navs[i].style.transform = 'translate(-100%)';
-				navs[i].style.transitionDuration = (navs.length - i + 1) * 0.05 + 's';
-			}
-		}
-	}
+    }
 }]);
--- scripts/services.js
+++
@@ -1,7 +0,0 @@
-
-/**
-* services Module
-*
-* 管理所有自定义服务
-*/
-angular.module('services', [])
